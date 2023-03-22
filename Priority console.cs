510f3dba906bb344d6d99b88f9559052edd6839f
vennela jangam <vennela.jangam@nagarro.com>
2023-03-22 14:10:49 +0530

    first commit

---
+++ Priority console.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+class PriorityQueue<T>
+{
+    private List<Tuple<T, int>> queue = new List<Tuple<T, int>>();
+
+    public void Enqueue(T item, int priority)
+    {
+        queue.Add(Tuple.Create(item, priority));
+    }
+
+    public T Dequeue()
+    {
+        int bestIndex = 0;
+
+        for (int i = 0; i < queue.Count; i++)
+        {
+            if (queue[i].Item2 < queue[bestIndex].Item2)
+            {
+                bestIndex = i;
+            }
+        }
+
+        T bestItem = queue[bestIndex].Item1;
+        queue.RemoveAt(bestIndex);
+        return bestItem;
+    }
+
+    public int Count
+    {
+        get { return queue.Count; }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        PriorityQueue<string> q = new PriorityQueue<string>();
+        q.Enqueue("venn", 2);
+        q.Enqueue("srem", 3);
+        q.Enqueue("lily", 1);
+
+        while (q.Count > 0)
+        {
+            Console.WriteLine(q.Dequeue());
+        }
+    }
+}
---
+++ priority Class.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class PriorityQueue<T>
+{
+    private SortedDictionary<int, Queue<T>> queue = new SortedDictionary<int, Queue<T>>();
+
+    public void Enqueue(T item, int priority)
+    {
+        if (!queue.ContainsKey(priority))
+        {
+            queue.Add(priority, new Queue<T>());
+        }
+        queue[priority].Enqueue(item);
+    }
+
+    public T Dequeue()
+    {
+        var highestPriority = queue.Keys.First();
+        var item = queue[highestPriority].Dequeue();
+        if (queue[highestPriority].Count == 0)
+        {
+            queue.Remove(highestPriority);
+        }
+        return item;
+    }
+
+    public int Count
+    {
+        get { return queue.Values.Sum(q => q.Count); }
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        PriorityQueue<string> q = new PriorityQueue<string>();
+
+        q.Enqueue("venn", 2);
+        q.Enqueue("srem", 3);
+        q.Enqueue("lily", 1);
+        
+
+        Console.WriteLine("Queue count: " + q.Count);
+
+        Console.WriteLine(q.Dequeue()); // should print task3
+        Console.WriteLine(q.Dequeue()); // should print task4
+        Console.WriteLine(q.Dequeue()); // should print task1
+        Console.WriteLine(q.Dequeue()); // should print task2
+
+        Console.WriteLine("Queue count: " + q.Count);
+    }
+}
